b195f7b078b1bbe572d54a34e0ff2672a9c02c18
Nmilien34 <Nm664@njit.edu>
2025-11-25 20:03:55 -0500

    fixing the backend

--- backend/src/models/CaregiverMessage.ts
+++ backend/src/models/CaregiverMessage.ts
@@ -54,7 +54,7 @@ const caregiverMessageSchema = new Schema<ICaregiverMessage>({
   timestamps: true,
   toJSON: {
     virtuals: true,
-    transform: function(doc, ret) {
+    transform: function(_doc, ret: any) {
       ret.id = ret._id.toString();
       delete ret._id;
       delete ret.__v;
